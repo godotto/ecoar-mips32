2bc455caf44cf2e0a226235e2947c46f6c2e9362
godotto <m.marcinkiewicz@protonmail.com>
2021-01-09 14:48:15 +0100

    Initialize main and define constants

---
+++ source/main.asm
@@ -0,0 +1,30 @@
+	.data
+	
+usageInfo:
+	.asciiz	"Usage instruction:\n\t<direction> <steps> to draw\n\tC <red> <green> <blue> to change colour\n\tq to end program\n"
+prompt:
+	.asciiz "Command: "
+errorInfo:
+	.asciiz "Unknown command\n"
+maxSize:
+	.byte	63
+	.align	1
+inputString:
+	.space	50
+colour:
+	.byte	0x0, 0x0, 0x0
+	.align 	0
+
+
+	.text
+	.globl main
+main:
+	li	$v0, 4		# print string syscall
+	la	$a0, usageInfo
+	syscall			# print usage info
+	
+
+	
+	
+	li	$v0, 10		# terminate execution syscall
+	syscall
